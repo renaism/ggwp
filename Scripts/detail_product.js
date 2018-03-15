355c1a0dc83aedf60a6c5f89a0568a8b6a08d84d
BayuGanteng <bayuarifat7@gmail.com>
2018-03-15 20:06:46 +0700

    baru3

---
+++ Scripts/detail_product.js
@@ -0,0 +1,18 @@
+function showOverview() {
+    $("#overviewTab").addClass("active");
+    $("#specificationsTab").removeClass("active");
+    $("#specifications").hide();
+    $("#overview").show();
+    $("#overview").ready();
+}
+
+function showSpecifications() {
+    $("#specificationsTab").addClass("active");
+    $("#overviewTab").removeClass("active");
+    $("#overview").hide();
+    $("#specifications").show();
+}
+
+$("document").ready(function() {
+    $("#specifications").hide();
+});
